66f47b57f47e056a860432d1d4296e3296694f75
Sofian Smimid <sofian.smimid@etu.unilim.fr>
2025-07-10 09:59:17 +0200

    Initial Commit

---
+++ MyCV/MyCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCVApp.swift
+//  MyCV
+//
+//  Created by Sofian Smimid on 10/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
